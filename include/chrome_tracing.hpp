e1f15eeaf79082764310b4737320d896adaeab4b
Ryuta Kono <otami0103@gmail.com>
2021-05-26 11:05:46 +0900

    Added measure time process

--- include/chrome_tracing.hpp
+++ include/chrome_tracing.hpp
@@ -3,12 +3,40 @@
 class ChromeTracing
 {
  public:
-    ChromeTracing();
-    ~ChromeTracing();
+    ChromeTracing()
+        : m_count(0)
+        , m_sum(0.0)
+    {
+    }
+    
+    ~ChromeTracing()
+    {
+    }
 
-    void   start();
-    double end();
-    double getAverageTime();
+    void start()
+    {
+        m_start = std::chrono::system_clock::now();
+    }
+    
+    double end()
+    {
+        auto end = std::chrono::system_clock::now();
+        double time = std::chrono::duration_cast<std::chrono::microseconds>(end - m_start).count() / 1000.0;
+
+        m_count++;
+        m_sum += time;
+
+        return time;
+    }
+    
+    double getAverage()
+    {
+        return m_sum / (double)m_count;
+    }
 
 private:
+    std::chrono::system_clock::time_point m_start;
+    
+    int    m_count;
+    double m_sum;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <unistd.h>
 #include <chrome_tracing.hpp>
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Hello World" << std::endl;
+    ChromeTracing tracing;
+
+    for (int i = 0; i < 10; i++) {
+        tracing.start();
+        
+        usleep(10000); // 10000 [us] = 10 [ms]
+        
+        double time = tracing.end();
+        std::cout << i << ": " << time << " [ms]" << std::endl;
+    }
+
+    std::cout << "Average: " << tracing.getAverage() << " [ms]" << std::endl;
     
     return 0;
 }
